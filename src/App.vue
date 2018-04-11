4af168f7a64d8930290ead1448355ef23fdd1042
Takami <mariko.takami.ext@siemens-healthineers.com>
2018-04-11 17:51:22 +0900

    tutorial1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/cat.jpg">
+    <h1>Todo Management.</h1>
+    <hr />
+    <router-view></router-view>
   </div>
 </template>
 
@@ -13,7 +15,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Meiryo UI', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
